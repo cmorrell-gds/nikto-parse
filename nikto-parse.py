d42320de772363b6585c18a80a9ac2bfb7e76bd4
Colin Morrell <cmorrell@gdssecurity.com>
2017-03-09 10:20:16 -0500

    update exception handler

--- nikto-parse.py
+++ nikto-parse.py
@@ -13,25 +13,24 @@ def main():
                         action='store', dest='string', required=True)
     args = parser.parse_args()
 
-    print '-'*80
-    print 'Hosts found for "%s..." in %s:' % (args.string[:30], args.directory) 
-    print '-'*80
-
     # enumerate filenames in the given dir
-    for filename in os.listdir(args.directory):
-        # open file handler on the each filename
-        with open(filename,'r') as data:
-            if args.string in data.read():
-                data.seek(0)
-                for line in data:
-                    if line.startswith('Host'):
-                        host = line.split(':')[1].strip()
-                    if line.startswith('Port'):
-                        port = line.split(':')[1].strip()
-                try:
-                    print '%s:%s' % (host, port)
-                except UnboundLocalError:
-                    print "error mothafucka"
+    try:
+        for filename in os.listdir(args.directory):
+            # open file handler on the each filename
+            with open(filename,'r') as data:
+                if args.string in data.read():
+                    data.seek(0)
+                    for line in data:
+                        if line.startswith('Host'):
+                            host = line.split(':')[1].strip()
+                        if line.startswith('Port'):
+                            port = line.split(':')[1].strip()
+                    try:
+                        print '%s:%s' % (host, port)
+                    except UnboundLocalError:
+                        pass
+    except IOError:
+        pass
 
 if __name__ == "__main__":
     main()
